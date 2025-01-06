df4454e453df355b95873b0141a5acc6e8168e84
Emir Özder <emir.ozder@id3.com.tr>
2025-01-07 00:47:12 +0300

    feat: products page

--- src/pages/productsPage/index.jsx
+++ src/pages/productsPage/index.jsx
@@ -1,5 +1,40 @@
+import { motion } from "framer-motion";
+import PageHeaderTitle from "../../components/PageTitleHeader";
+import StatCard from "../../components/StatCard";
+import { PRODUCTS_STAT_CARD_ITEMS } from "../../utils/consts";
+import ProductsTable from "./_partials/ProductsTable";
+import SalesProductsDistributionChart from "./_partials/SalesProductsDistributionChart";
+import SalesTrendChart from "./_partials/SalesTrendChart";
 const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+  return (
+    <div className="flex-1 overflow-auto relative z-10">
+      {/* PAGE TITLE */}
+      <PageHeaderTitle title="Products" />
+
+      <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
+        {/* STAT CARDS */}
+        <motion.div
+          className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          {PRODUCTS_STAT_CARD_ITEMS.map((item, index) => (
+            <StatCard key={index} {...item} />
+          ))}
+        </motion.div>
+
+        {/* PRODUCTS TABLE */}
+        <ProductsTable />
+
+        {/* CHARTS */}
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+          <SalesTrendChart />
+          <SalesProductsDistributionChart />
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default ProductsPage;
--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,4 +1,4 @@
-import { BarChart2, DollarSign, Percent, Settings, ShoppingBag, ShoppingCart, TrendingUp, Users } from "lucide-react";
+import { AlertTriangle, BarChart2, DollarSign, Package, Percent, Settings, ShoppingBag, ShoppingCart, TrendingUp, Users } from "lucide-react";
 
 export const SIDEBAR_ITEMS = [
   {
@@ -22,7 +22,7 @@ export const SIDEBAR_ITEMS = [
   {
     name: 'Sales',
     icon: DollarSign,
-    color: '#6D214F',
+    color: '#10B981',
     link: '/sales',
   },
   {
@@ -148,4 +148,168 @@ export const SALES_CHANNEL_CHART_DATA = [
   { name: "In-Store", value: 31400 },
   { name: "Mail Order", value: 24900 },
   { name: "Others", value: 18800 },
+];
+
+export const PRODUCTS_STAT_CARD_ITEMS = [
+  {
+    title: 'Total Products',
+    value: 12500,
+    icon: Package,
+    color: '#6366F1',
+  },
+  {
+    title: 'Top Selling',
+    value: 2500,
+    icon: TrendingUp,
+    color: '#2E93A1',
+  },
+  {
+    title: 'Low Stock',
+    value: 1500,
+    icon: AlertTriangle,
+    color: '#F59E0B',
+  },
+  {
+    title: 'Total Revenue',
+    value: '$250,000',
+    icon: DollarSign,
+    color: '#10B981',
+  },
+];
+
+export const SALES_TREND_CHART_DATA = [
+  {
+    name: "Jan",
+    sales: 4000,
+  },
+  {
+    name: "Feb",
+    sales: 3000,
+  },
+  {
+    name: "Mar",
+    sales: 2000,
+  },
+  {
+    name: "Apr",
+    sales: 2780,
+  },
+  {
+    name: "May",
+    sales: 1890,
+  },
+  {
+    name: "Jun",
+    sales: 2390,
+  },
+  {
+    name: "Jul",
+    sales: 3490,
+  },
+  {
+    name: "Aug",
+    sales: 4000,
+  },
+  {
+    name: "Sep",
+    sales: 3000,
+  },
+  {
+    name: "Oct",
+    sales: 2000,
+  },
+  {
+    name: "Nov",
+    sales: 2780,
+  },
+  {
+    name: "Dec",
+    sales: 1890,
+  },
+];
+
+export const SALES_PRODUCTS_DISTRIBUTION_CHART_COLORS = [
+  "#6366F1",
+  "#F43F5E",
+  "#F2A365",
+  "#9CA3AF",
+  "#EC4899",
+  "#4ADE80",
+  "#F59E0B",
+];
+
+export const SALES_PRODUCTS_DISTRIBUTION_CHART_DATA = [
+  { name: 'Steel Watch', value: 3490 },
+  { name: 'Leather Bag', value: 4000 },
+  { name: 'Cofee Machine', value: 3000 },
+  { name: 'Soft Pillow', value: 2000 },
+  { name: 'Wood Chair', value: 2780 },
+  { name: 'Cotton T-Shirt', value: 1890 },
+  { name: 'Glass Table', value: 2390 },
+];
+
+export const PRODUCTS_TABLE_DATA = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1451290173668-9a14beaf714c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c3RlZWwlMjB3YXRjaHxlbnwwfHwwfHx8MA%3D%3D",
+    name: "Steel Watch",
+    category: "Fashion",
+    price: 99.99,
+    stock: 340,
+    sales: 3490
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1622560480654-d96214fdc887?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGxlYXRoZXIlMjBiYWd8ZW58MHx8MHx8fDA%3D",
+    name: "Leather Bag",
+    category: "Fashion",
+    price: 99.99,
+    stock: 430,
+    sales: 4000
+  },
+  {
+    id: 3,
+    img: "https://plus.unsplash.com/premium_photo-1664189122777-01ad20e7fb9a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29mZmVlJTIwbWFjaGluZXxlbnwwfHwwfHx8MA%3D%3D",
+    name: "Cofee Machine",
+    category: "Home & Kitchen",
+    price: 149.99,
+    stock: 560,
+    sales: 3000
+  },
+  {
+    id: 4,
+    img: "https://images.unsplash.com/photo-1730580015629-d07e58d81c32?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c29mdCUyMHBpbGxvd3xlbnwwfHwwfHx8MA%3D%3D",
+    name: "Soft Pillow",
+    category: "Home & Kitchen",
+    price: 19.99,
+    stock: 890,
+    sales: 2000
+  },
+  {
+    id: 5,
+    img: "https://images.unsplash.com/photo-1699588772787-1eed3b726e0a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHdvb2QlMjBjaGFpcnxlbnwwfHwwfHx8MA%3D%3D",
+    name: "Wood Chair",
+    category: "Furniture",
+    price: 199.99,
+    stock: 120,
+    sales: 2780
+  },
+  {
+    id: 6,
+    img: "https://plus.unsplash.com/premium_photo-1673125287363-b4e837f1215f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y290dG9uJTIwdHNoaXJ0fGVufDB8fDB8fHww",
+    name: "Cotton T-Shirt",
+    category: "Fashion",
+    price: 29.99,
+    stock: 2000,
+    sales: 1890
+  },
+  {
+    id: 7,
+    img: "https://images.unsplash.com/photo-1563146413-d915a569d6b1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Z2xhc3MlMjB0YWJsZXxlbnwwfHwwfHx8MA%3D%3D",
+    name: "Glass Table",
+    category: "Furniture",
+    price: 299.99,
+    stock: 50,
+    sales: 2390
+  },
 ];
