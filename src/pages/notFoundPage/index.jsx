9df5d4f474032a74f4184bb0a76fe2e580646781
Emir Özder <emir.ozder@id3.com.tr>
2025-01-15 00:16:53 +0300

    fix: not found page

--- src/pages/notFoundPage/index.jsx
+++ src/pages/notFoundPage/index.jsx
@@ -1,5 +1,12 @@
+import { AlertTriangle } from "lucide-react";
+
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <div className="flex flex-1 justify-center items-center h-screen overflow-auto relative z-10 font-bold text-4xl text-gray-300">
+      <AlertTriangle className="w-16 h-16 mr-4" />
+      Page Not Found!
+    </div>
+  );
 };
 
 export default NotFoundPage;
