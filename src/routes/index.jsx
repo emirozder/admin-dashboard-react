bde1cf15db8c139cb28ca0e64603b6fbb526dc92
Emir Özder <emir.ozder@id3.com.tr>
2025-01-15 13:28:15 +0300

    fix: imports and package lock

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import AnalyticsPage from "../pages/AnalyticsPage/AnalyticsPage";
-import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
-import OrdersPage from "../pages/OrdersPage/OrdersPage";
-import OverviewPage from "../pages/OverviewPage/OverviewPage";
-import ProductsPage from "../pages/ProductsPage/ProductsPage";
-import SalesPage from "../pages/SalesPage/SalesPage";
-import SettingsPage from "../pages/SettingsPage/SettingsPage";
-import UsersPage from "../pages/UsersPage/UsersPage";
+import AnalyticsPage from "../pages/AnalyticsPage/AnalyticsPage.jsx";
+import NotFoundPage from "../pages/NotFoundPage/NotFoundPage.jsx";
+import OrdersPage from "../pages/OrdersPage/OrdersPage.jsx";
+import OverviewPage from "../pages/OverviewPage/OverviewPage.jsx";
+import ProductsPage from "../pages/ProductsPage/ProductsPage.jsx";
+import SalesPage from "../pages/SalesPage/SalesPage.jsx";
+import SettingsPage from "../pages/SettingsPage/SettingsPage.jsx";
+import UsersPage from "../pages/UsersPage/UsersPage.jsx";
 
 const AppRoutes = () => {
   return (
